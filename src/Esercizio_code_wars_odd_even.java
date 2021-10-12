9534fbc233b74189ced339f284026364eaae50b0
Baidoc-Alexandru <alexandru.baidoc@itiszuccante.edu.it>
2021-10-12 22:01:55 +0000

    Esercizio_code_wars_odd_even

---
+++ src/Esercizio_code_wars_odd_even.java
@@ -0,0 +1,14 @@
+public class Esercizio_code_wars_odd_even {
+    public static void main(String[]args){
+        //inserire il numero intero desiderato
+        int numero1;
+        //inserire il numero intero desiderato
+        int numero2;
+        //sommare i numeri inseriti
+        System.out.println(numero1+numero2);
+        //verificare se la somma e pari o dispari
+
+
+
+    }
+}
